08852e44f4edb58ad4353c94e21de57c613cb914
omkarvijay5 <omkarvijay5@gmail.com>
2014-06-12 16:30:57 +0530

    Completed 20 lessons in python koans and 20th lesson is on about_scoring_project.py

---
+++ koans/triangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Triangle Project Code.
+
+
+# triangle(a, b, c) analyzes the lengths of the sides of a triangle
+# (represented by a, b and c) and returns the type of triangle.
+#
+# It returns:
+#   'equilateral'  if all sides are equal
+#   'isosceles'    if exactly 2 sides are equal
+#   'scalene'      if no sides are equal
+#
+# The tests for this method can be found in
+#   about_triangle_project.py
+# and
+#   about_triangle_project_2.py
+#
+def triangle(a, b, c):
+    if a+b > c and a+c > b and b+c > a:
+        if a == b == c:
+            return 'equilateral'
+        elif a == b or a == c or b == c:
+            return 'isosceles'
+        else:
+            return 'scalene'        
+    elif a == b == c == 0:
+        raise TriangleError()
+    elif a < 0 or b < 0 or c < 0:
+        raise TriangleError()
+    else:
+        raise TriangleError()
+# Error class used in part 2.  No need to change this code.
+class TriangleError(StandardError):
+    pass
